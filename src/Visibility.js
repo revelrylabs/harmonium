72d15c05d0a96dad52ece194224db0cfe508ef79
jwietelmann <jwietelmann@gmail.com>
2016-10-14 14:01:20 -0400

    visibility component

---
+++ src/Visibility.js
@@ -0,0 +1,45 @@
+import React, {Component, PropTypes, cloneElement} from 'react'
+import classNames from 'classnames'
+
+const CLASS_NAMES = {
+  showForSmall: 'show-for-small',
+  showForMedium: 'show-for-medium',
+  showForLarge: 'show-for-large',
+
+  showForSmallOnly: 'show-for-small-only',
+  showForMediumOnly: 'show-for-medium-only',
+  showForLargeOnly: 'show-for-large-only',
+
+  hideForSmall: 'hide-for-small',
+  hideForMedium: 'hide-for-medium',
+  hideForLarge: 'hide-for-large',
+
+  hideForSmallOnly: 'hide-for-small-only',
+  hideForMediumOnly: 'hide-for-medium-only',
+  hideForLargeOnly: 'hide-for-large-only',
+}
+
+const PROP_TYPES = {
+  children: PropTypes.element,
+}
+Object.keys(CLASS_NAMES).forEach(key => PROP_TYPES[key] = PropTypes.bool)
+
+export default class Visibility extends Component {
+
+  static propTypes = PROP_TYPES;
+
+  render() {
+    const classNamesList = [this.props.className]
+    Object.keys(this.props).forEach((propName) => {
+      const className = CLASS_NAMES[propName]
+      if(className) {
+        classNamesList.push(className)
+      }
+    })
+
+    const newClassName = classNames(classNamesList)
+    return (
+      cloneElement(this.props.children, {className: newClassName})
+    )
+  }
+}
