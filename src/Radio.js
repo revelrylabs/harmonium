7491b8b116bd72a2c7253146839a08b675523331
Brian Ng <bng@revelry.co>
2016-11-18 16:52:18 -0600

    radio

--- src/Radio.js
+++ src/Radio.js
@@ -1,8 +1,38 @@
-import React from 'react'
-import Input from './Input'
+import React, {Component, PropTypes} from 'react'
 
 export default class Radio extends React.Component {
+  static propTypes = {
+    name: PropTypes.string,
+    value: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+    ]),
+    label: PropTypes.string,
+    className: PropTypes.string,
+    checked: PropTypes.bool,
+    onRadioClick: PropTypes.func,
+  }
+
+  get radioButton() {
+    return <input
+      className={this.inputClasses}
+      type="radio"
+      name={this.props.name}
+      value={this.props.value}
+      onClick={this.handleClick}
+    />
+  }
+
+  handleClick = () => {
+    this.props.onRadioClick(this.props.value)
+  }
+
   render() {
-    return <Input {...this.props} type="radio" />
+    return (
+      <label className={`RevInput-radio ${this.props.className}`}>
+        {this.radioButton}
+        <span className="RevInput-radio-copy">{this.props.label}</span>
+      </label>
+    )
   }
 }
