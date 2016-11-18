c0dc9b37877b2490c904780a7264c27a5d354ffd
Sia <siakaramalegos@gmail.com>
2016-11-18 15:54:45 -0600

    Adds more explanation to InputHelpText example

--- src/InputHelpText.example.js
+++ src/InputHelpText.example.js
@@ -6,11 +6,17 @@ export class Example extends Component {
   render() {
     return (
       <div>
-        <p>Example with no help text:</p>
+        <p>
+          Example with no help text (it returns null when empty so nothing is there!):
+        </p>
         <InputHelpText />
+
+        <hr />
 
         <p>Example with help text as a string:</p>
         <InputHelpText>I am some very helpful text.'</InputHelpText>
+
+        <hr />
 
         <p>Example with help text as children:</p>
         <InputHelpText>
