b97edb21bc9ceb0d083fb215f86d7ce73004e1f7
Sia <siakaramalegos@gmail.com>
2016-11-18 15:38:07 -0600

    Adds jsdom for testing with mount

--- test-config.js
+++ test-config.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import {expect} from 'chai'
 import {mount, shallow} from 'enzyme'
+import {jsdom} from 'jsdom'
+
+global.document = jsdom('')
+global.window = document.defaultView
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    global[property] = document.defaultView[property]
+  }
+})
+
+global.navigator = {
+  userAgent: 'node.js'
+}
 
 global.expect = expect
 global.mount = mount
