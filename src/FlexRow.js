1edc85cd424eca08931291c66b5895259d3c157b
harleymckee <h@harley.website>
2016-11-18 15:56:26 -0600

    cleanup FlexRow

--- src/FlexRow.js
+++ src/FlexRow.js
@@ -1,33 +1,38 @@
 import React, {PropTypes} from 'react'
 import {Row} from './grid'
 import classNames from 'classnames'
-import _str from 'underscore.string'
 
-const BOOL_PROPS = [
-  'collapse',
-  'smallUnstack',
-  'mediumUnstack',
-  'largeUnstack',
-  'alignRight',
-  'alignCenter',
-  'alignJustify',
-  'alignSpaced',
-  'alignTop',
-  'alignMiddle',
-  'alignStretch',
-  'smallCollapse',
-  'mediumCollapse',
-  'largeCollapse',
-  'smallUncollapse',
-  'mediumUncollapse',
-  'largeUncollapse',
-]
+const BOOL_PROP_MAPPINGS = {
+  collapse: 'collapse',
+  smallUnstack: 'small-unstack',
+  mediumUnstack: 'medium-unstack',
+  largeUnstack: 'large-unstack',
+  alignRight: 'align-right',
+  alignCenter: 'align-center',
+  alignJustify: 'align-justify',
+  alignSpaced: 'align-spaced',
+  alignTop: 'align-top',
+  alignMiddle: 'align-middle',
+  alignStretch: 'align-stretch',
+  smallCollapse: 'small-collapse',
+  mediumCollapse: 'medium-collapse',
+  largeCollapse: 'large-collapse',
+  smallUncollapse: 'small-uncollapse',
+  mediumUncollapse: 'medium-uncollapse',
+  largeUncollapse: 'large-uncollapse',
+}
 
-const NUMBER_PROPS = [
-  'smallUp',
-  'mediumUp',
-  'largeUp',
-]
+const NUMBER_PROP_MAPPINGS = {
+  small: (x) => `small-${x}`,
+  medium: (x) => `medium-${x}`,
+  large: (x) => `large-${x}`,
+  smallUp: (x) => `small-up-${x}`,
+  mediumUp: (x) => `medium-up-${x}`,
+  largeUp: (x) => `large-up-${x}`,
+}
+
+const NUMBER_PROPS = Object.keys(NUMBER_PROP_MAPPINGS)
+const BOOL_PROPS = Object.keys(BOOL_PROP_MAPPINGS)
 
 const PROP_TYPES = {
   children: PropTypes.element,
@@ -49,48 +54,34 @@ NUMBER_PROPS.forEach((x) => PROP_TYPES[x] = React.PropTypes.oneOf([
   12, '12',
 ]))
 
-const CLASS_NAMES = {
-  'RevRow': true,
-  'row': true,
+function getBoolClassNames(props) {
+  return BOOL_PROPS
+    .filter((prop) => props[prop])
+    .map((prop) => BOOL_PROP_MAPPINGS[prop])
+}
 
-  collapse: 'collapse',
-  smallUnstack: 'small-unstack',
-  mediumUnstack: 'medium-unstack',
-  largeUnstack: 'large-unstack',
-  alignRight: 'align-right',
-  alignCenter: 'align-center',
-  alignJustify: 'align-justify',
-  alignSpaced: 'align-spaced',
-  alignTop: 'align-top',
-  alignMiddle: 'align-middle',
-  alignStretch: 'align-stretch',
-  smallCollapse: 'small-collapse',
-  mediumCollapse: 'medium-collapse',
-  largeCollapse: 'large-collapse',
-  smallUncollapse: 'small-uncollapse',
-  mediumUncollapse: 'medium-uncollapse',
-  largeUncollapse: 'large-uncollapse',
+function getNumberClassNames(props) {
+  return NUMBER_PROPS
+    .filter((prop) => props[prop])
+    .map((prop) => NUMBER_PROP_MAPPINGS[prop](props[prop]))
 }
 
 export default class FlexRow extends Row {
-  static propTypes = PROP_TYPES;
-
   get className() {
-    let numberClassNamesObject = {}
-
-    NUMBER_PROPS.forEach((x) => {
-      let count = this.props[x]
-      if(count != null) {
-        numberClassNamesObject[`${_str.dasherize(x)}-${count}`] = true
-      }
-    })
-
-    return classNames(this.props.className, Object.assign({}, CLASS_NAMES, numberClassNamesObject))
+    return classNames(
+      'row revRow',
+      this.props.className,
+      getBoolClassNames(this.props),
+      getNumberClassNames(this.props)
+    )
   }
 
   render() {
+    console.log('dingus')
     return <div {...this.props} className={this.className}>
       {this.props.children}
     </div>
   }
 }
+
+FlexRow.propTypes = PROP_TYPES
