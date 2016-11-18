582a420f9b7c74afc086728f5ed3157ebcb92259
Brian Ng <bng@revelry.co>
2016-11-18 17:11:53 -0600

    merge develop

---
+++ docs-src/__generated__.js
@@ -0,0 +1,86 @@
+// GENERATED FILE. DON'T EDIT IT, STUPID.
+import React, {createElement} from 'react'
+import {render} from 'react-dom'
+import {Example1 as BrowserSupportWarning_examples_Example1} from '../src/BrowserSupportWarning.example'
+import {Example2 as BrowserSupportWarning_examples_Example2} from '../src/BrowserSupportWarning.example'
+import {Styles as Button_examples_Styles} from '../src/Button.example'
+import {Styles as ButtonBar_examples_Styles} from '../src/ButtonBar.example'
+import {Styles as ButtonGroup_examples_Styles} from '../src/ButtonGroup.example'
+import {Example as Callout_examples_Example} from '../src/Callout.example'
+import {Example as CloseButton_examples_Example} from '../src/CloseButton.example'
+import {GridExample as Col_examples_GridExample} from '../src/Col.example'
+import {Example as Currency_examples_Example} from '../src/Currency.example'
+import {Empty as Emptyable_examples_Empty} from '../src/Emptyable.example'
+import {NonEmpty as Emptyable_examples_NonEmpty} from '../src/Emptyable.example'
+import {Example1 as HelpText_examples_Example1} from '../src/HelpText.example'
+import {Example2 as HelpText_examples_Example2} from '../src/HelpText.example'
+import {Example as Icon_examples_Example} from '../src/Icon.example'
+import {Example as Input_examples_Example} from '../src/Input.example'
+import {Styles as Label_examples_Styles} from '../src/Label.example'
+import {Paragraph1 as Lipsum_examples_Paragraph1} from '../src/Lipsum.example'
+import {Paragraph2 as Lipsum_examples_Paragraph2} from '../src/Lipsum.example'
+import {Paragraph3 as Lipsum_examples_Paragraph3} from '../src/Lipsum.example'
+import {Basics as MediaObject_examples_Basics} from '../src/MediaObject.example'
+import {SectionAlignment as MediaObject_examples_SectionAlignment} from '../src/MediaObject.example'
+import {Stack as MediaObject_examples_Stack} from '../src/MediaObject.example'
+import {NestedMediaObjects as MediaObject_examples_NestedMediaObjects} from '../src/MediaObject.example'
+import {Example1 as Modal_examples_Example1} from '../src/Modal.example'
+import {Example1 as Pagination_examples_Example1} from '../src/Pagination.example'
+import {Example2 as Pagination_examples_Example2} from '../src/Pagination.example'
+import {Example3 as Pagination_examples_Example3} from '../src/Pagination.example'
+import {Example1 as Pluralize_examples_Example1} from '../src/Pluralize.example'
+import {Example2 as Pluralize_examples_Example2} from '../src/Pluralize.example'
+import {Example3 as Pluralize_examples_Example3} from '../src/Pluralize.example'
+import {Example4 as Pluralize_examples_Example4} from '../src/Pluralize.example'
+import {Styles as Radio_examples_Styles} from '../src/Radio.example'
+import {GridExample as Row_examples_GridExample} from '../src/Row.example'
+import {Example as Select_examples_Example} from '../src/Select.example'
+import {Stateful as Tabs_examples_Stateful} from '../src/Tabs.example'
+import {Example1 as TextAlign_examples_Example1} from '../src/TextAlign.example'
+import {Example as Textarea_examples_Example} from '../src/Textarea.example'
+import {Example1 as Visibility_examples_Example1} from '../src/Visibility.example'
+import {MetaPackage as grid_examples_MetaPackage} from '../src/grid.example'
+const $ = (x) => document.getElementById(x)
+const mount = (component, id) => $(id) && render(createElement(component), $(id))
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('mounting components...')
+  mount(BrowserSupportWarning_examples_Example1, 'BrowserSupportWarning_examples_Example1')
+  mount(BrowserSupportWarning_examples_Example2, 'BrowserSupportWarning_examples_Example2')
+  mount(Button_examples_Styles, 'Button_examples_Styles')
+  mount(ButtonBar_examples_Styles, 'ButtonBar_examples_Styles')
+  mount(ButtonGroup_examples_Styles, 'ButtonGroup_examples_Styles')
+  mount(Callout_examples_Example, 'Callout_examples_Example')
+  mount(CloseButton_examples_Example, 'CloseButton_examples_Example')
+  mount(Col_examples_GridExample, 'Col_examples_GridExample')
+  mount(Currency_examples_Example, 'Currency_examples_Example')
+  mount(Emptyable_examples_Empty, 'Emptyable_examples_Empty')
+  mount(Emptyable_examples_NonEmpty, 'Emptyable_examples_NonEmpty')
+  mount(HelpText_examples_Example1, 'HelpText_examples_Example1')
+  mount(HelpText_examples_Example2, 'HelpText_examples_Example2')
+  mount(Icon_examples_Example, 'Icon_examples_Example')
+  mount(Input_examples_Example, 'Input_examples_Example')
+  mount(Label_examples_Styles, 'Label_examples_Styles')
+  mount(Lipsum_examples_Paragraph1, 'Lipsum_examples_Paragraph1')
+  mount(Lipsum_examples_Paragraph2, 'Lipsum_examples_Paragraph2')
+  mount(Lipsum_examples_Paragraph3, 'Lipsum_examples_Paragraph3')
+  mount(MediaObject_examples_Basics, 'MediaObject_examples_Basics')
+  mount(MediaObject_examples_SectionAlignment, 'MediaObject_examples_SectionAlignment')
+  mount(MediaObject_examples_Stack, 'MediaObject_examples_Stack')
+  mount(MediaObject_examples_NestedMediaObjects, 'MediaObject_examples_NestedMediaObjects')
+  mount(Modal_examples_Example1, 'Modal_examples_Example1')
+  mount(Pagination_examples_Example1, 'Pagination_examples_Example1')
+  mount(Pagination_examples_Example2, 'Pagination_examples_Example2')
+  mount(Pagination_examples_Example3, 'Pagination_examples_Example3')
+  mount(Pluralize_examples_Example1, 'Pluralize_examples_Example1')
+  mount(Pluralize_examples_Example2, 'Pluralize_examples_Example2')
+  mount(Pluralize_examples_Example3, 'Pluralize_examples_Example3')
+  mount(Pluralize_examples_Example4, 'Pluralize_examples_Example4')
+  mount(Radio_examples_Styles, 'Radio_examples_Styles')
+  mount(Row_examples_GridExample, 'Row_examples_GridExample')
+  mount(Select_examples_Example, 'Select_examples_Example')
+  mount(Tabs_examples_Stateful, 'Tabs_examples_Stateful')
+  mount(TextAlign_examples_Example1, 'TextAlign_examples_Example1')
+  mount(Textarea_examples_Example, 'Textarea_examples_Example')
+  mount(Visibility_examples_Example1, 'Visibility_examples_Example1')
+  mount(grid_examples_MetaPackage, 'grid_examples_MetaPackage')
+})
