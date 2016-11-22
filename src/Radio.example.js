0db86115bb3e7f0cf44fedf6058855eed3c71d13
jwietelmann <jwietelmann@gmail.com>
2016-11-22 12:41:28 -0500

    Deprecate Option and add more examples

---
+++ src/Radio.example.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+import {Row, Col} from './grid'
+import Radio from './Radio'
+
+const OPTIONS = [
+  {label: 'North', value: 'N'},
+  {label: 'South', value: 'S'},
+  {label: 'East', value: 'E'},
+  {label: 'West', value: 'W'},
+]
+
+export class Singular extends Component {
+  render() {
+    return (
+      <Row>
+        <Col>
+          <Radio name="singular" defaultValue="1" label="One" />
+        </Col>
+        <Col>
+          <Radio name="singular" defaultValue="2" label="Has help text" help="This is help text." />
+        </Col>
+        <Col>
+          <Radio name="singular" defaultValue="3" label="Has error" error="This is an error." />
+        </Col>
+        <Col>
+          <Radio name="singular" defaultValue="2" label="Has both" help="This is help text." error="This is an error." />
+        </Col>
+      </Row>
+    )
+  }
+}
+
+export class Fieldset extends Component {
+  render() {
+    return (
+      <Row>
+        <Col>
+          <Radio.Fieldset options={OPTIONS} label="Has help text" help="This is the help text." />
+        </Col>
+        <Col>
+          <Radio.Fieldset options={OPTIONS} label="Has error" error="This is the error." />
+        </Col>
+        <Col>
+          <Radio.Fieldset options={OPTIONS} label="Stacks both help and error" help="This is the help text." error="This is the error." />
+        </Col>
+      </Row>
+    )
+  }
+}
--- src/Select.example.js
+++ src/Select.example.js
@@ -1,14 +1,59 @@
 import React, {Component} from 'react'
+import {Row, Col} from './grid'
 import Select from './Select'
-import Option from './Option'
 
-export class Example extends Component {
+// Got tired of writing explicit options for examples that aren't
+// demonstrating options specifically.
+const OPTIONS = [
+  {label: 'North', value: 'N'},
+  {label: 'South', value: 'S'},
+  {label: 'East', value: 'E'},
+  {label: 'West', value: 'W'},
+]
+
+export class OptionChildrenVsOptionsProp extends Component {
   render() {
     return (
-      <Select name="airport" label="Airport">
-        <Option value="MSY">Louis Armstrong</Option>
-        <Option value="JFK">John F. Kennedy</Option>
-      </Select>
+      <Row>
+        <Col>
+          <Select name="airport" label="Airport with <option> children.">
+            <option value="MSY">Louis Armstrong</option>
+            <option value="JFK">John F. Kennedy</option>
+          </Select>
+        </Col>
+        <Col>
+          <Select name="airport" label="Airport with `options` prop." options={[
+            {label: 'Louis Armstrong', value: 'MSY'},
+            {label: 'John F. Kennedy', value: 'JFK'},
+          ]} />
+        </Col>
+      </Row>
+    )
+  }
+}
+
+export class MultipleValues extends Component {
+  render() {
+    return (
+      <Select options={OPTIONS} defaultValue={['S', 'E']} multiple />
+    )
+  }
+}
+
+export class HelpTextAndError extends Component {
+  render() {
+    return (
+      <Row>
+        <Col>
+          <Select options={OPTIONS} label="Has help text" help="This is the help text." />
+        </Col>
+        <Col>
+          <Select options={OPTIONS} label="Has error" error="This is the error." />
+        </Col>
+        <Col>
+          <Select options={OPTIONS} label="Stacks both help and error" help="This is the help text." error="This is the error." />
+        </Col>
+      </Row>
     )
   }
 }
