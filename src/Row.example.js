92c1137789bd21172be28f984d3354f5bf27dce7
jwietelmann <jwietelmann@gmail.com>
2016-11-22 18:45:42 -0500

    better grid example

--- src/Row.example.js
+++ src/Row.example.js
@@ -1,15 +1,45 @@
 import React, {Component} from 'react'
 import {Row, Col} from './grid'
 
+const X = () => <div style={{border: '1px dotted #999'}}>X</div>
+
 export class GridExample extends Component {
   render() {
     return (
       <Row>
         <Col>
+          <Row><Col><strong>small</strong></Col></Row>
           <Row>
-            <Col small={4}>4</Col>
-            <Col small={4}>4</Col>
-            <Col small={4}>4</Col>
+            <Col small={4}><X /></Col>
+            <Col small={4}><X /></Col>
+            <Col small={4}><X /></Col>
+          </Row>
+          <Row>
+            <Col small={3}><X /></Col>
+            <Col small={4}><X /></Col>
+            <Col small={5}><X /></Col>
+          </Row>
+          <Row><Col><strong>medium</strong></Col></Row>
+          <Row>
+            <Col medium={4}><X /></Col>
+            <Col medium={4}><X /></Col>
+            <Col medium={4}><X /></Col>
+          </Row>
+          <Row>
+            <Col medium={3}><X /></Col>
+            <Col medium={4}><X /></Col>
+            <Col medium={5}><X /></Col>
+          </Row>
+          <Row><Col><strong>large</strong></Col></Row>
+          <Row>
+            <Col large={4}><X /></Col>
+            <Col large={4}><X /></Col>
+            <Col large={4}><X /></Col>
+          </Row>
+          <Row>
+            <Col large={3}><X /></Col>
+            <Col large={4}><X /></Col>
+            <Col large={5}><X /></Col>
           </Row>
         </Col>
       </Row>
