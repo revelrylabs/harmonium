6665664a6bdc516f453bb2e10fa4d7b430dd42b4
jwietelmann <jwietelmann@gmail.com>
2016-11-23 12:43:59 -0500

    grid improvements

--- src/Row.js
+++ src/Row.js
@@ -1,31 +1,61 @@
-import React from 'react'
+import React, {Component} from 'react'
 import classNames from 'classnames'
 
+const BOOL_PROPS_TO_CLASS_NAMES = {
+  collapse: 'collapse',
+
+  smallCollapse: 'small-collapse',
+  mediumCollapse: 'medium-collapse',
+  largeCollapse: 'large-collapse',
+
+  smallUncollapse: 'small-uncollapse',
+  mediumUncollapse: 'medium-uncollapse',
+  largeUncollapse: 'large-uncollapse',
+}
+
+const BOOL_PROPS = Object.keys(BOOL_PROPS_TO_CLASS_NAMES)
+
+const NUMBER_PROPS_TO_CLASS_NAMES = {
+  smallUp: (x) => `small-up-${x}`,
+  mediumUp: (x) => `medium-up-${x}`,
+  largeUp: (x) => `large-up-${x}`,
+}
+
+const NUMBER_PROPS = Object.keys(NUMBER_PROPS_TO_CLASS_NAMES)
+
 export default class Row extends React.Component {
-
-  static get propTypes() {
-    return {
-      collapse: React.PropTypes.bool,
-    }
-  }
-
-  static get defaultProps() {
-    return {
-      collapse: false,
-    }
-  }
-
-  get className() {
-    return classNames(this.props.className, {
-      'RevRow': true,
-      'row': true,
-      'collapse': this.props.collapse,
-    })
-  }
-
   render() {
-    return <div {...this.props} className={this.className}>
-      {this.props.children}
-    </div>
+    const {children, className, ...props} = this.props
+
+    const boolClassNames = []
+    BOOL_PROPS.forEach((name) => {
+      if(props[name]) {
+        boolClassNames.push(BOOL_PROPS_TO_CLASS_NAMES[name] )
+      }
+      delete props[name]
+    })
+
+    const numberClassNames = []
+    NUMBER_PROPS.forEach((name) => {
+      const value = props[name]
+      const fn = NUMBER_PROPS_TO_CLASS_NAMES[name]
+      if(value != null) {
+        numberClassNames.push(fn(value))
+      }
+      delete props[name]
+    })
+
+    const divClassName = classNames(
+      className,
+      'row',
+      boolClassNames,
+      numberClassNames,
+    )
+
+    return (
+      <div {...props} className={divClassName}>
+        {children}
+      </div>
+    )
   }
 }
--- src/grid.example.js
+++ src/grid.example.js
@@ -1,10 +1,211 @@
 import React, {Component} from 'react'
-import {Row, Col} from './grid'
 
-export class MetaPackage extends Component {
+// For the sake of this demo, we're changing them to make them more visible.
+// For real world use, just `import {Row, Col} from './grid'` or whatever.
+import {Row as OriginalRow, Col as OriginalCol} from './grid'
+const style = {backgroundColor: 'rgba(0,0,0,0.05)'}
+const Row = (props) => <OriginalRow {...props} style={style}>{props.children}</OriginalRow>
+const Col = (props) => <OriginalCol {...props} style={style}>{props.children}</OriginalCol>
+
+const X = () => <div style={{border: '1px dotted #999'}}>X</div>
+
+export class Basic extends Component {
   render() {
     return (
-      <strong>This is merely a metapackage comprising Row and Col.</strong>
+      <Row>
+        <Col>
+          <Row><Col><strong>small</strong></Col></Row>
+          <Row>
+            <Col small={4}><X /></Col>
+            <Col small={4}><X /></Col>
+            <Col small={4}><X /></Col>
+          </Row>
+          <Row>
+            <Col small={3}><X /></Col>
+            <Col small={4}><X /></Col>
+            <Col small={5}><X /></Col>
+          </Row>
+          <Row><Col><strong>medium</strong></Col></Row>
+          <Row>
+            <Col medium={4}><X /></Col>
+            <Col medium={4}><X /></Col>
+            <Col medium={4}><X /></Col>
+          </Row>
+          <Row>
+            <Col medium={3}><X /></Col>
+            <Col medium={4}><X /></Col>
+            <Col medium={5}><X /></Col>
+          </Row>
+          <Row><Col><strong>large</strong></Col></Row>
+          <Row>
+            <Col large={4}><X /></Col>
+            <Col large={4}><X /></Col>
+            <Col large={4}><X /></Col>
+          </Row>
+          <Row>
+            <Col large={3}><X /></Col>
+            <Col large={4}><X /></Col>
+            <Col large={5}><X /></Col>
+          </Row>
+        </Col>
+      </Row>
+    )
+  }
+}
+
+export class Nesting extends Component {
+  render() {
+    return (
+      <Row>
+        <Col small={8}>8
+          <Row>
+            <Col small={8}>8 Nested
+              <Row>
+                <Col small={8}>8 Nested Again</Col>
+                <Col small={4}>4</Col>
+              </Row>
+            </Col>
+            <Col small={4}>4</Col>
+          </Row>
+        </Col>
+        <Col small={4}>4</Col>
+      </Row>
+    )
+  }
+}
+
+export class Offsets extends Component {
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col large={1}>1</Col>
+          <Col large={11}>11</Col>
+        </Row>
+        <Row>
+          <Col large={1}>1</Col>
+          <Col large={10} largeOffset={1}>10, offset 1</Col>
+        </Row>
+        <Row>
+          <Col large={1}>1</Col>
+          <Col large={9} largeOffset={2}>9, offset 2</Col>
+        </Row>
+        <Row>
+          <Col large={1}>1</Col>
+          <Col large={8} largeOffset={3}>8, offset 3</Col>
+        </Row>
+      </div>
+    )
+  }
+}
+
+export class IncompleteRows extends Component {
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col medium={3}>3</Col>
+          <Col medium={3}>3</Col>
+          <Col medium={3}>3</Col>
+        </Row>
+        <Row>
+          <Col medium={3}>3</Col>
+          <Col medium={3}>3</Col>
+          <Col medium={3} end>3 end</Col>
+        </Row>
+      </div>
+    )
+  }
+}
+
+export class RowCollapse extends Component {
+  render() {
+    return (
+      <Row mediumUncollapse largeUncollapse>
+        <Col small={6}>
+          Removes gutter at large media query
+        </Col>
+        <Col small={6}>
+          Removes gutter at large media query
+        </Col>
+      </Row>
+    )
+  }
+}
+
+export class CenteredColumns extends Component {
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col small={3} smallCentered>3 centered</Col>
+        </Row>
+        <Row>
+          <Col small={6} smallCentered>6 centered</Col>
+        </Row>
+        <Row>
+          <Col small={9} smallCentered largeUncentered>9 centered</Col>
+        </Row>
+        <Row>
+          <Col small={11} smallCentered>11 centered</Col>
+        </Row>
+      </div>
+    )
+  }
+}
+
+export class SourceOrdering extends Component {
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col small={10} smallPush={2}>10</Col>
+          <Col small={2} smallPull={10}>2, last</Col>
+        </Row>
+        <Row>
+          <Col large={9} largePush={3}>9</Col>
+          <Col large={3} largePull={9}>3, last</Col>
+        </Row>
+        <Row>
+          <Col large={8} largePush={4}>8</Col>
+          <Col large={4} largePull={8}>4, last</Col>
+        </Row>
+        <Row>
+          <Col small={5} smallPush={7}>7</Col>
+          <Col small={7} smallPull={5}>5, last</Col>
+        </Row>
+        <Row>
+          <Col medium={6} mediumPush={6}>6</Col>
+          <Col medium={6} mediumPull={6}>6, last</Col>
+        </Row>
+      </div>
+    )
+  }
+}
+
+export class BlockGrids extends Component {
+  render() {
+    return (
+      <Row smallUp={1} mediumUp={2} largeUp={4}>
+        <Col>
+          <img src="//placehold.it/300x300" className="thumbnail" alt="" />
+        </Col>
+        <Col>
+          <img src="//placehold.it/300x300" className="thumbnail" alt="" />
+        </Col>
+        <Col>
+          <img src="//placehold.it/300x300" className="thumbnail" alt="" />
+        </Col>
+        <Col>
+          <img src="//placehold.it/300x300" className="thumbnail" alt="" />
+        </Col>
+        <Col>
+          <img src="//placehold.it/300x300" className="thumbnail" alt="" />
+        </Col>
+        <Col>
+          <img src="//placehold.it/300x300" className="thumbnail" alt="" />
+        </Col>
+      </Row>
     )
   }
 }
