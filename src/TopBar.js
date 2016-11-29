ada01d669c44e9a5d76d1633584d4734e60a107f
jwietelmann <jwietelmann@gmail.com>
2016-11-29 10:49:26 -0500

    add TopBar

---
+++ src/TopBar.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react'
+import classNames from 'classnames'
+
+const BOOL_PROPS_TO_CLASS_NAMES = {
+  stackedForMedium: 'stacked-for-medium',
+  stackedForLarge: 'stacked-for-large',
+}
+const BOOL_PROPS = Object.keys(BOOL_PROPS_TO_CLASS_NAMES)
+
+export default class TopBar extends Component {
+  render() {
+    const {className, children, ...props} = this.props
+    const propClassNames = []
+    BOOL_PROPS.forEach((name) => {
+      if(props[name]) {
+        propClassNames.push(BOOL_PROPS_TO_CLASS_NAMES[name])
+      }
+      delete props[name]
+    })
+    const newClassName = classNames(className, 'top-bar')
+    return (
+      <div {...props} className={newClassName}>
+        {children}
+      </div>
+    )
+  }
+}
+
+class TopBarLeft extends Component {
+  render() {
+    const {className, children, ...props} = this.props
+    const newClassName = classNames(className, 'top-bar-left')
+    return (
+      <div {...props} className={newClassName}>
+        {children}
+      </div>
+    )
+  }
+}
+TopBar.Left = TopBarLeft
+
+class TopBarRight extends Component {
+  render() {
+    const {className, children, ...props} = this.props
+    const newClassName = classNames(className, 'top-bar-right')
+    return (
+      <div {...props} className={newClassName}>
+        {children}
+      </div>
+    )
+  }
+}
+TopBar.Right = TopBarRight
+
+class TopBarTitle extends Component {
+  render() {
+    const {className, children, ...props} = this.props
+    const newClassName = classNames(className, 'top-bar-title')
+    return (
+      <div className={newClassName}>
+        {children}
+      </div>
+    )
+  }
+}
+TopBar.Title = TopBarTitle
