88e59c6e3656a69bdc59176f58644028c5369350
jwietelmann <jwietelmann@gmail.com>
2016-11-29 19:33:56 -0500

    add FlexVideo

---
+++ src/FlexVideo.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import classNames from 'classnames'
+
+export default class FlexVideo extends Component {
+  render() {
+    const {className, children, widescreen, vimeo, ...props} = this.props
+
+    const divClassName = classNames(className, 'flex-video', {
+      widescreen,
+      vimeo,
+    })
+
+    return (
+      <div {...props} className={divClassName}>
+        {children}
+      </div>
+    )
+  }
+}
