c6795792d456fc5ca97715593e65b90428d736cd
jwietelmann <jwietelmann@gmail.com>
2016-11-30 15:13:38 -0500

    suitify BrowserSupportWarning

--- src/BrowserSupportWarning.js
+++ src/BrowserSupportWarning.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react'
 import bowser from 'bowser'
+import classNames from 'classnames'
 
 export default class BrowserSupportWarning extends Component {
   render() {
-    const {minVersions, children} = this.props
+    const {minVersions, className, children} = this.props
     const userAgent = this.props.userAgent || (typeof(window) !== 'undefined' && window.navigator.userAgent) || null
     const isSupported = userAgent ? bowser.check(this.props.minVersions, this.userAgent) : true
     if(isSupported) {
       return null
     }
-    return <div>{this.props.children}</div>
+    const divClassName = classNames(className, 'rev-BrowserSupportWarning')
+    return <div className={divClassName}>{this.props.children}</div>
   }
 }
