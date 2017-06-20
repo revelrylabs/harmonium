c5c79e49c763b29e4dcacfaac9bfdccf7f85de93
CJ Horton <thecjhorton@gmail.com>
2017-06-20 11:19:07 -0500

    yay tests

--- src/AuthenticityToken.test.js
+++ src/AuthenticityToken.test.js
@@ -4,4 +4,17 @@ describe('AuthenticityToken', () => {
   it('should render without throwing', () => {
     shallow(<AuthenticityToken />)
   })
+
+  it('should have a name and value', () => {
+    const authenticityTokenName = "token"
+    const authenticityTokenValue = "no hackers plz"
+
+    const authenticityToken = shallow(<input name={authenticityTokenName} value={authenticityTokenValue} />)
+    const renderedName = authenticityToken.first().prop('name')
+    const renderedValue = authenticityToken.first().prop('value')
+    
+    expect(renderedName).to.equal(authenticityTokenName)
+    expect(renderedValue).to.equal(authenticityTokenValue)
+  })
+
 })
