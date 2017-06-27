fd0a12b3e3caa186abd528e7489bde5158a5cd3a
CJ Horton <thecjhorton@gmail.com>
2017-06-26 19:25:18 -0500

    tweak postinstall message

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,7 +1,9 @@
-const knownIssue = `\nKNOWN ISSUE: Deprecation warnings for minimatch and graceful-fs
-are coming from Gulp and shouldn't cause problems. They'll be fixed in Gulp 4.
-For further info, see https://github.com/gulpjs/gulp/issues/1571\n\n
-To remove this warning, delete scripts/postinstall.js and 
-remove the reference in package.json > "scripts" > "postinstall"\n`
+const knownIssue = `\nDon't worry about the deprecation warnings for minimatch and graceful-fs.
+They're coming from Gulp and will be fixed in Gulp 4.
+For further info, see https://github.com/gulpjs/gulp/issues/1571`
 
-console.log('\x1b[34m%s\x1b[0m', knownIssue)
+const howToRemove = `(To remove this warning, delete scripts/postinstall.js 
+and remove the reference in package.json > "scripts" > "postinstall")\n`
+
+console.log('\x1b[34m%s', knownIssue)
+console.log('\x1b[32m%s\x1b[0m', howToRemove)
