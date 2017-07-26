d6880edcfd2145b9bf9bd46ceac5b3c2cc3d2f97
Robert Prehn <robert@revelry.co>
2017-07-26 16:41:37 -0500

    Add test coverage for I-M

--- src/Modal.test.js
+++ src/Modal.test.js
@@ -1,7 +1,25 @@
 import Modal from './Modal'
+import sinon from 'sinon'
 
 describe('Modal', () => {
   it('should render without throwing', () => {
     shallow(<Modal><h2>Some Content</h2></Modal>)
+  })
+
+  it('handles background clicks without onBackgroundClick prop', () => {
+    const modal = shallow(<Modal isOpen><h2>Some Content</h2></Modal>)
+
+    modal.prop('onBackgroundClick')()
+    expect(modal.state('isOpen')).to.eq(false)
+  })
+
+  it('handles background clicks with onBackgroundClick prop', () => {
+    const spy = sinon.spy()
+    const modal = shallow(<Modal isOpen onBackgroundClick={spy}><h2>Some Content</h2></Modal>)
+
+    modal.prop('onBackgroundClick')()
+
+    expect(model.state('isOpen')).to.eq(false)
+    expect(spy.called).to.eq(true)
   })
 })
