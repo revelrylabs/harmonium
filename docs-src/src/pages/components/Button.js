45382f292e0ea198caabe86366d6fd3685d137a1
Robert Prehn <robert@revelry.co>
2017-08-08 12:55:48 -0500

    Clean up and bugfix examples A & B

--- docs-src/src/pages/components/Button.js
+++ docs-src/src/pages/components/Button.js
@@ -3,7 +3,13 @@ import ExampleSection from '../../ExampleSection'
 import scope from '../../ExampleScope'
 
 const examples = {
-  'Button': require('raw!../../examples/Button.js.example')
+  'Sizes': require('raw!../../examples/Button/Sizes.js.example'),
+  'Types': require('raw!../../examples/Button/Types.js.example'),
+  'Types (Disabled)': require('raw!../../examples/Button/DisabledTypes.js.example'),
+  'Hollow': require('raw!../../examples/Button/Hollow.js.example'),
+  'Expanded': require('raw!../../examples/Button/Expanded.js.example'),
+  'Button with Form Attributes': require('raw!../../examples/Button/FormAttributes.js.example'),
+  'Buttons with href prop become anchor links': require('raw!../../examples/Button/LinkButton.js.example'),
 }
 
 export default class ButtonExamplePage extends Component {
