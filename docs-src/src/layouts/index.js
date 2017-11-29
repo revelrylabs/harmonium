4ee22fe5ab18bc56b05c6723e6720396c2059a81
Laura Hill <laura@revelry.co>
2017-11-29 16:56:26 -0600

    The start of some layout work

--- docs-src/src/layouts/index.js
+++ docs-src/src/layouts/index.js
@@ -3,14 +3,19 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import {Row, Col} from 'revelry-components/lib/grid'
+import TopBar from 'revelry-components/lib/TopBar'
 import Navigation from './Navigation'
 
 import './index.scss'
 
 const Header = () => (
-  <header className="rev-Header">
-    <Link to="/">Ockham</Link>
-  </header>
+  <TopBar fixed>
+    <Row>
+      <Col>
+        <Link to="/">Ockham</Link>
+      </Col>
+    </Row>
+  </TopBar>
 )
 
 const TemplateWrapper = ({
@@ -28,7 +33,7 @@ const TemplateWrapper = ({
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.0.0/theme/monokai.min.css"/>
       </Helmet>
       <Header />
-      <div className="rev-Split">
+      <div className="rev-Split rev-ContentWrapper">
         <Navigation />
         <div className="rev-MainPanel">
           <main className="rev-MainPanel-content">
