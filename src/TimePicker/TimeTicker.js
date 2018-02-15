edfb2cf554b245d3ce5e5e7781e49f4756ae5500
RyanMillerMath <ryan.miller@365connect.com>
2018-02-14 20:22:36 -0600

    Create base template for time ticker

--- src/TimePicker/TimeTicker.js
+++ src/TimePicker/TimeTicker.js
@@ -2,22 +2,38 @@
 
 import classNames from 'classnames'
 import React, { createElement } from 'react'
-import Input from '../Input'
+import Button from '../Button'
 
 /**
  * A component which increments or decrements a unit of time,
  * e.g. (01-12 for hours, 00-59 for minutes, and AM/PM)
  * @param {object} props - the props of the TimeTicker
  */
-const TimeTicker = ({
-  className,
-  ...props
-}) => {
-  return (
-    <div>
-      TimeTicker
-    </div>
-  )
+
+ // TODO: Generalize this to a general incrementor/decrementor
+ // component that can cycle through a range of numbers or an enum
+ export default class TimeTicker extends React.Component {
+  
+  render () {
+    const {
+      className,
+      ...props
+    } = this.props
+
+    return (
+      <div {...props} className={`rev-TimeTicker ${className}`}>
+        <Button className="rev-TimeTicker-button--previous">
+        </Button>
+
+        <div className="rev-TimeTicker-value">
+          {/* current enum value */}
+        </div>
+
+        <Button className="rev-TimeTicker-button--next">
+        </Button>
+      </div>
+    )
+  }
 }
 
 export default TimeTicker
