bf8776e6d8812b9496261874a35715320c50e613
RyanMillerMath <ryan.miller@365connect.com>
2018-02-17 22:12:15 -0600

    Implement base structure for time ticker

--- src/TimePicker/TimeTicker.js
+++ src/TimePicker/TimeTicker.js
@@ -13,27 +13,49 @@ import Button from '../Button'
  // TODO: Generalize this to a general incrementor/decrementor
  // component that can cycle through a range of numbers or an enum
  export default class TimeTicker extends React.Component {
+  /**
+   * The default values for props of this component
+   * @return {object} the default value object
+   */
+  static get defaultProps() {
+    const createElement = React.createElement
+
+    return {
+      previousLabel: <span>&#708;</span>,
+      nextLabel: <span>&#709;</span>,
+    }
+  }
+
+  /**
+   * Creates a container for the time tickers. Sets state.time to the values of the tickers
+   * @param {object} props
+   */
+  constructor(props) {
+    super(props)
+  }
   
   render () {
     const {
       className,
+      previousLabel,
+      nextLabel,
       ...props
     } = this.props
 
     return (
       <div {...props} className={`rev-TimeTicker ${className}`}>
         <Button className="rev-TimeTicker-button--previous">
+          {previousLabel}
         </Button>
 
         <div className="rev-TimeTicker-value">
-          {/* current enum value */}
+          0
         </div>
 
         <Button className="rev-TimeTicker-button--next">
+          {nextLabel}
         </Button>
       </div>
     )
   }
 }
-
-export default TimeTicker
