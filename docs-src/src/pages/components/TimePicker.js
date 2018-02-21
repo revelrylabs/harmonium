5f0e482a2adb8118aff385be64a8ea4d4e7787d7
RyanMillerMath <ryan.miller@365connect.com>
2018-02-21 17:30:52 -0600

    Add all examples

--- docs-src/src/pages/components/TimePicker.js
+++ docs-src/src/pages/components/TimePicker.js
@@ -3,7 +3,13 @@ import ExampleSection from '../../ExampleSection'
 import scope from '../../ExampleScope'
 
 const examples = {
-  'Basic': require('raw!../../examples/TimePicker/Basic.js.example')
+  'Without Props': require('raw!../../examples/TimePicker/Basic.js.example'),
+  'With Default Value': require('raw!../../examples/TimePicker/DefaultValue.js.example'),
+  'With Error and Help': require('raw!../../examples/TimePicker/ErrorAndHelp.js.example'),
+  'Always Open': require('raw!../../examples/TimePicker/AlwaysOpen.js.example'),
+  'Disabled': require('raw!../../examples/TimePicker/Disabled.js.example'),
+  'With 24 Hour Clock': require('raw!../../examples/TimePicker/TwentyFourHour.js.example'),
+  'With Seconds Shown': require('raw!../../examples/TimePicker/ShowSeconds.js.example')
 }
 
 export default class TimePickerExamplePage extends Component {
