64247c8273bbf1a32ad95463d270c670e4c00d9d
RyanMillerMath <ryan.miller@365connect.com>
2018-03-13 19:23:48 -0500

    Revert "temp commit"
    This reverts commit dd6e40428f4030ec837d8f53a85cb04623479d88.

--- src/Sticky.test.js
+++ src/Sticky.test.js
@@ -1,5 +1,4 @@
-import Sticky, { StatefulSticky } from './Sticky'
-import jsdom from 'jsdom'
+import Sticky, {Sticky} from './Sticky'
 import sinon from 'sinon'
 
 describe('Sticky', () => {
@@ -22,33 +21,8 @@ describe('Sticky', () => {
   })
 })
 
-const win = {
-  scrollBy(x, y) {
-    window.scrollBy(x, y);
-  }
-}
-
 describe('Sticky.Stateful', () => {
   it('should render without throwing', () => {
     shallow(<Sticky.Stateful><span>test</span></Sticky.Stateful>)
-  })
-
-  it('sticks the children of a container while scrolling past the container', () => {
-    let spy = sinon.spy()
-    let component = mount(<Sticky.Stateful><span>test</span></Sticky.Stateful>)
-    component.instance().componentDidMount()
-
-    jsdom()
-
-    while(!component.state('isStuck')) {
-      win.scrollBy(0, y)
-      y++
-    }
-
-    console.log(component.state('isStuck'))
-    console.log(component.update().state('isStuck'))
-
-    // expect(component.update().state('isStuck')['1']).to.eq(true)
-    expect(spy.called).to.eq(true)
   })
 })
