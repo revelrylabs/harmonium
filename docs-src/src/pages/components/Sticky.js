6974dbd82f8e7d760ed2652623a4179e4a0f66e3
Robert Prehn <robert@revelry.co>
2018-05-31 11:22:37 -0500

    chore: Update meta tags on doc site pages

--- docs-src/src/pages/components/Sticky.js
+++ docs-src/src/pages/components/Sticky.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import ExampleSection from '../../ExampleSection'
 import scope from '../../ExampleScope'
+import Headers from '../../Headers'
 
 const examples = {
   'Stateless': require('raw!../../examples/Sticky/Stateless.js.example'),
@@ -12,8 +13,35 @@ const examples = {
 
 export default class StickyExamplePage extends Component {
   render() {
-    return <div>
-      <ExampleSection title="Sticky" examples={examples} depth={1} scope={scope} />
-    </div>
+    return (
+      <div>
+        <Headers
+          title="Sticky"
+          metaDescription={
+            'The Sticky component is a container that "sticks" to an edge of the ' +
+            'browser window as it is scrolled. It is typically used for navigation ' +
+            'elements or important messages that should remain in sight as the user ' +
+            'scrolls. It should be used sparingly only when the contents are ' +
+            'important, because it takes up space and distracts the user.'
+          }
+          extraKeywords="Component, Sticky, Layout, Navigation"
+        >
+          <p>
+            The Sticky component is a container that "sticks" to an edge of the
+            browser window as it is scrolled. It is typically used for
+            navigation elements or important messages that should remain in
+            sight as the user scrolls. It should be used sparingly only when the
+            contents are important, because it takes up space and distracts the
+            user.
+          </p>
+        </Headers>
+        <ExampleSection
+          title="Sticky"
+          examples={examples}
+          depth={1}
+          scope={scope}
+        />
+      </div>
+    )
   }
 }
