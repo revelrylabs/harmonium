1fe5532b06a7ec437f6865a87df1a90b51e56367
Blaze Barsamian <blaze@revelry.co>
2018-07-19 10:54:05 -0500

    Table updates

--- docs-src/src/pages/components/InputGroupTables/InputGroupProps.js
+++ docs-src/src/pages/components/InputGroupTables/InputGroupProps.js
@@ -20,112 +20,112 @@ export default function InputGroupProps() {
           </Table.Header>
         </Table.Row>
       </Table.Head>
-      <Table.HeadSmall>
+      <Table.HeadStacked>
         <Table.Data>
           Input Group Props
         </Table.Data>
-      </Table.HeadSmall>
+      </Table.HeadStacked>
       <Table.Body>
         <Table.Row>
           <Table.Data>
-            <span>Name:</span> <code>secondary</code>
+            <Table.HeaderInline>Name:</Table.HeaderInline> <code>secondary</code>
           </Table.Data>
           <Table.Data>
-            <span>Type:</span> bool
+            <Table.HeaderInline>Type:</Table.HeaderInline> bool
           </Table.Data>
           <Table.Data>
-            <span>Default:</span> false
+            <Table.HeaderInline>Default:</Table.HeaderInline> false
           </Table.Data>
           <Table.Data>
-            <span>Description:</span> Prop used to change button style in InputGroup.
+            <Table.HeaderInline>Description:</Table.HeaderInline> Prop used to change button style in InputGroup.
           </Table.Data>
         </Table.Row>
         <Table.Row>
           <Table.Data>
-            <span>Name:</span> <code>placeholder</code>
+            <Table.HeaderInline>Name:</Table.HeaderInline> <code>placeholder</code>
           </Table.Data>
           <Table.Data>
-            <span>Type:</span> string
+            <Table.HeaderInline>Type:</Table.HeaderInline> string
           </Table.Data>
           <Table.Data>
-            <span>Default:</span>
+            <Table.HeaderInline>Default:</Table.HeaderInline>
           </Table.Data>
           <Table.Data>
-            <span>Description:</span> Prop used to display a short hint in the input before the user enters a value.
-          </Table.Data>
-        </Table.Row>
-
-        <Table.Row>
-          <Table.Data>
-            <span>Name:</span> <code>error</code>
-          </Table.Data>
-          <Table.Data>
-            <span>Type:</span> bool, string
-          </Table.Data>
-          <Table.Data>
-            <span>Default:</span>
-          </Table.Data>
-          <Table.Data>
-            <span>Description:</span> Prop added for input errors.
+            <Table.HeaderInline>Description:</Table.HeaderInline> Prop used to display a short hint in the input before the user enters a value.
           </Table.Data>
         </Table.Row>
 
         <Table.Row>
           <Table.Data>
-            <span>Name:</span> <code>label</code>
+            <Table.HeaderInline>Name:</Table.HeaderInline> <code>error</code>
           </Table.Data>
           <Table.Data>
-            <span>Type:</span> string
+            <Table.HeaderInline>Type:</Table.HeaderInline> bool, string
           </Table.Data>
           <Table.Data>
-            <span>Default:</span>
+            <Table.HeaderInline>Default:</Table.HeaderInline>
           </Table.Data>
           <Table.Data>
-            <span>Description:</span> Prop added for input label.
-          </Table.Data>
-        </Table.Row>
-
-        <Table.Row>
-          <Table.Data>
-            <span>Name:</span> <code>help</code>
-          </Table.Data>
-          <Table.Data>
-            <span>Type:</span> string
-          </Table.Data>
-          <Table.Data>
-            <span>Default:</span>
-          </Table.Data>
-          <Table.Data>
-            <span>Description:</span> Prop added for input help text.
+            <Table.HeaderInline>Description:</Table.HeaderInline> Prop added for input errors.
           </Table.Data>
         </Table.Row>
 
         <Table.Row>
           <Table.Data>
-            <span>Name:</span> <code>type</code>
+            <Table.HeaderInline>Name:</Table.HeaderInline> <code>label</code>
           </Table.Data>
           <Table.Data>
-            <span>Type:</span> 
+            <Table.HeaderInline>Type:</Table.HeaderInline> string
           </Table.Data>
           <Table.Data>
-            <span>Default:</span>
+            <Table.HeaderInline>Default:</Table.HeaderInline>
           </Table.Data>
           <Table.Data>
-            <span>Description:</span> Prop added for input type.
+            <Table.HeaderInline>Description:</Table.HeaderInline> Prop added for input label.
+          </Table.Data>
+        </Table.Row>
+
+        <Table.Row>
+          <Table.Data>
+            <Table.HeaderInline>Name:</Table.HeaderInline> <code>help</code>
+          </Table.Data>
+          <Table.Data>
+            <Table.HeaderInline>Type:</Table.HeaderInline> string
+          </Table.Data>
+          <Table.Data>
+            <Table.HeaderInline>Default:</Table.HeaderInline>
+          </Table.Data>
+          <Table.Data>
+            <Table.HeaderInline>Description:</Table.HeaderInline> Prop added for input help text.
+          </Table.Data>
+        </Table.Row>
+
+        <Table.Row>
+          <Table.Data>
+            <Table.HeaderInline>Name:</Table.HeaderInline> <code>type</code>
+          </Table.Data>
+          <Table.Data>
+            <Table.HeaderInline>Type:</Table.HeaderInline> 
+          </Table.Data>
+          <Table.Data>
+            <Table.HeaderInline>Default:</Table.HeaderInline>
+          </Table.Data>
+          <Table.Data>
+            <Table.HeaderInline>Description:</Table.HeaderInline> Prop added for input type.
           </Table.Data>
         </Table.Row>
         <Table.Row>
           <Table.Data>
-            <span>Name:</span> <code>defaultValue</code>
+            <Table.HeaderInline>Name:</Table.HeaderInline> <code>defaultValue</code>
           </Table.Data>
           <Table.Data>
-            <span>Type:</span>
+            <Table.HeaderInline>Type:</Table.HeaderInline>
           </Table.Data>
           <Table.Data>
-            <span>Default:</span>
+            <Table.HeaderInline>Default:</Table.HeaderInline>
           </Table.Data>
           <Table.Data>
-            <span>Description:</span> Prop added for
+            <Table.HeaderInline>Description:</Table.HeaderInline> Prop added for
           </Table.Data>
         </Table.Row>
       </Table.Body>
