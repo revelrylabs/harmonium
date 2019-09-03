a938c775d818202a214dbaa2e40a8c221eabae14
Jake Kiernan <kiernan.jake@gmail.com>
2019-09-02 20:24:16 -0500

    test: adding slider test

--- src/Slider.test.js
+++ src/Slider.test.js
@@ -6,4 +6,18 @@ describe('Slider', () => {
   it('should render without throwing', () => {
     expect(() => shallow(<Slider/>)).not.to.throw()
   })
+  it('can render slider with initial value and change the value', () => {
+    const wrapper = mount(
+      <Slider
+        min={0}
+        max={10}
+        step={2}
+        initialValue={4}
+        name={'Test Slider'}
+      />
+    )
+    expect(wrapper.exists()).to.equal(true)
+    wrapper.find('.rev-Slider-range').instance().value = 4;
+    wrapper.find('.rev-Slider-range').simulate("change", { target: { value: 8 }}).instance().value = 8;
+  })
 })
