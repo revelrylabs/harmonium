0217803bcb093d82694e7a13201494ebee2e2d79
Jake Kiernan <kiernan.jake@gmail.com>
2019-09-02 20:23:11 -0500

    test: adding close button test

--- src/CloseButton.test.js
+++ src/CloseButton.test.js
@@ -6,4 +6,12 @@ describe('CloseButton', () => {
   it('should render without throwing', () => {
     shallow(<CloseButton />)
   })
+  it('can click close button', () => {
+    let count = 0
+    const mockCallBack = () => count++
+    const button = shallow(<CloseButton onClick={mockCallBack}>Close</CloseButton>)
+    expect(count).to.equal(0);
+    button.find('button').simulate('click');
+    expect(count).to.equal(1);
+  })
 })
