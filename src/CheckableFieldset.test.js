35de87a74624ed6af86ce7840d09be874c5bc70b
Jake Kiernan <kiernan.jake@gmail.com>
2019-09-02 20:20:49 -0500

    test: adding checkable fieldset test

--- src/CheckableFieldset.test.js
+++ src/CheckableFieldset.test.js
@@ -13,4 +13,19 @@ describe('CheckableFieldset', () => {
       </CheckableFieldset>
     )
   })
+  it('can text of three checkboxes', () => {
+    const wrapper = mount(
+      <CheckableFieldset label="Poll">
+        <Checkbox name="poll" defaultValue="1" label="One" className="firstCheckbox" />
+        <Checkbox name="poll" defaultValue="2" label="Two" className="secondCheckbox"/>
+        <Checkbox name="poll" defaultValue="3" label="Three" className="thirdCheckbox" />
+      </CheckableFieldset>
+    )
+    expect(wrapper.find('.firstCheckbox').exists()).to.equal(true);
+    expect(wrapper.find('.firstCheckbox').find('.rev-Checkbox-label').text()).to.equal('One');
+    expect(wrapper.find('.secondCheckbox').exists()).to.equal(true);
+    expect(wrapper.find('.secondCheckbox').find('.rev-Checkbox-label').text()).to.equal('Two');
+    expect(wrapper.find('.thirdCheckbox').exists()).to.equal(true);
+    expect(wrapper.find('.thirdCheckbox').find('.rev-Checkbox-label').text()).to.equal('Three');
+  })
 })
