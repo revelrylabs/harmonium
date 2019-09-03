f863c8a7647337ec3198ce290683b7ac296af8ca
Jake Kiernan <kiernan.jake@gmail.com>
2019-09-02 20:21:57 -0500

    test: adding input group test

--- src/InputGroup.test.js
+++ src/InputGroup.test.js
@@ -18,4 +18,22 @@ describe('InputGroup', () => {
       </InputGroup.Stack>
     )
   })
+  it('can render errors and helper text', () => {
+    const wrapper = mount(
+      <InputGroup.Stack
+        label="InputGroupTest"
+        help="The help text for it"
+        error="The error message"
+      >
+        <InputGroup.Label>$USD</InputGroup.Label>
+        <InputGroup.Field>
+          <Input type="number" defaultValue={100.0} error="The error message" />
+        </InputGroup.Field>
+      </InputGroup.Stack>
+    )
+    expect(wrapper.find('InputErrors').exists()).to.equal(true);
+    expect(wrapper.find('InputErrors').text()).to.equal('The error message');
+    expect(wrapper.find('InputHelpText').exists()).to.equal(true);
+    expect(wrapper.find('InputHelpText').text()).to.equal('The help text for it');
+  })
 })
