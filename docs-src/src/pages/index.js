f3d1a94193b0472e484bd92b95b386ab78bf7914
Bryan Joseph <bryanjos@users.noreply.github.com>
2020-01-23 14:46:05 -0600

    chore: Update installation instructions
    * Add more info for using Harmonium with React
    * Add info on using Harmonium without React
    * Update Layout so that Helmet component isn't needed in child
      components to add description

--- docs-src/src/pages/index.js
+++ docs-src/src/pages/index.js
@@ -1,42 +1,24 @@
 import React from 'react'
+import { useEffect } from "react"
 import Row from 'harmonium/lib/Row';
 import Col from 'harmonium/lib/Col';
-import Card from 'harmonium/lib/Card'
-import Menu from 'harmonium/lib/Menu'
-import HelpText from 'harmonium/lib/HelpText'
-import Helmet from 'react-helmet'
 import Layout from '../layouts/index.js'
+import Prism from "prismjs"
 
-const IndexPage = ({location}) => (
-  <Layout location={location}>
+const description = 'Harmonium is a framework of React components optimized for ' +
+'teams that want to ship apps fast. It is a curated list of ' +
+'components that work together and have cohesive styles. One ' +
+'of our design goals is that you never have to research and ' +
+'handpick component packages. Whatever you need is already here.'
+
+const IndexPage = ({location}) => {
+  useEffect(() => {
+    // call the highlightAll() function to style our code blocks
+    Prism.highlightAll()
+  })
+
+  return <Layout location={location} description={description}>
     <Row>
-      <Helmet
-        title={'Harmonium | React Components for Teams That Move Fast'}
-        meta={[
-          {
-            name: 'description',
-            content:
-              'Harmonium is a framework of React components optimized for ' +
-              'teams that want to ship apps fast. It is a curated list of ' +
-              'components that work together and have cohesive styles. One ' +
-              'of our design goals is that you never have to research and ' +
-              'handpick component packages. Whatever you need is already here.',
-          },
-          {
-            name: 'keywords',
-            content: 'React, JavaScript, SCSS, CSS, HTML, Web',
-          },
-        ]}
-      >
-        <link
-          rel="stylesheet"
-          href="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.0.0/codemirror.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.38.0/theme/lucario.min.css"
-        />
-      </Helmet>
       <Col>
         <h1 className="ShowForSR">
           Hello Harmonium&mdash; The React Framework for Shipping Fast
@@ -62,16 +44,69 @@ const IndexPage = ({location}) => (
         </p>
         <h2>Installation</h2>
         <pre>
-          <code>npm install --save harmonium</code>
+          <code className="language-bash">npm install --save harmonium</code>
         </pre>
+        <h2>Using Harmonium with React</h2>
+        <pre>
+            <code className="language-jsx">
+              {`import React, {Component} from 'react'
+import Row from 'harmonium/lib/Row';
+import Col from 'harmonium/lib/Col';
+import Button from 'harmonium/lib/Button';
+
+export default class MyComponent extends Component {
+
+  render() {
+    return <Row>
+    <Col>
+      <h3>Hello, world</h3>
+    </Col>
+    <Col>
+      <Button small>Click here</Button>
+    </Col>
+    </Row>
+  }
+}
+`}
+            </code>
+          </pre>
         <p>
-          You can now import the components and start using them. See the
-          component example documentation listed to the left.
+          For more, see the component documentation listed to the left.
         </p>
-        <h2>SCSS</h2>
+        <h2>Using Harmonium without React</h2>
         <p>
-          We don&apos;t just provide JavaScript. We&apos;ve built a set of SCSS
-          styles for all the components. It is in the <code>scss</code> directory
+          We don&apos;t just provide React components. We&apos;ve built a set of SCSS
+          styles for all the components.
+        </p>
+        <p>
+          To include Harmonium's styles into your scss, import it like so
+        </p>
+        <pre>
+            <code className="language-scss">
+              {`/* myapp.scss */
+@import '~harmonium/scss/app';
+/* your styles here; */
+`}
+            </code>
+          </pre>
+        <p>
+          You can import the variable settings as well to override them
+        </p>
+          <pre>
+            <code className="language-scss">
+              {`/* myapp.scss */
+@import '~harmonium/settings-templates/color-palette';
+@import '~harmonium/settings-templates/harmonium-settings';
+@import '~harmonium/scss/app';
+
+$brand: #eeeeee;
+
+/* your styles here; */
+`}
+            </code>
+          </pre>
+        <p>
+          For advanced configurations, styles can be found in the <code>scss</code> directory
           of the package. You can either copy it into your project&apos;s SCSS
           directory, or use a tool like{' '}
           <a href="https://www.npmjs.com/package/sassy-npm-importer">
@@ -88,6 +123,6 @@ const IndexPage = ({location}) => (
       </Col>
     </Row>
   </Layout>
-)
+}
 
 export default IndexPage
