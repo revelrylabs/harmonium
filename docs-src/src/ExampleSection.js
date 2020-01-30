a29b2768699ddc73e11ff7503f300e9564fc9f9e
Bryan Joseph <bryanjos@users.noreply.github.com>
2020-01-30 08:39:12 -0600

    Remove disableHTMLPreview prop. Using Tabs component in ExampleSection

--- docs-src/src/ExampleSection.js
+++ docs-src/src/ExampleSection.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {Row, Col} from 'harmonium/lib/grid'
+import Tabs from 'harmonium/lib/Tabs'
 import prettier from 'prettier/standalone';
 import prettierHTML from 'prettier/parser-html'
 import {
@@ -38,45 +39,29 @@ function HTML({ live }) {
 const LiveHTML = withLive(HTML);
 
 export default function ExampleSection(props) {
-  const [active, setActive] = useState(1);
-
   if (typeof props.examples === 'string') {
     return (
       <div className="example-section HTMLCodeSection">
         <LiveProvider theme={theme} code={props.examples} scope={props.scope}>
           <LiveError />
           <LivePreview className="example-preview" />
-          <div className="rev-Tabs">
-        <ul className="rev-Tabs-titles">
-          <li className={`rev-TabsTitle ${active === 1 && "rev-TabsTitle--selected"}`}>
-            <a className="rev-TabsTitle-link" onClick={() => setActive(1)}>React</a>
-          </li>
-          <li className={`rev-TabsTitle ${active === 2 && "rev-TabsTitle--selected"}`}>
-            <a className="rev-TabsTitle-link" onClick={() => setActive(2)}>HTML</a>
-          </li>
-        </ul>
-        <div className="rev-Tabs-content">
-          <div
-            className={`rev-TabsItem-panel ${active === 1 && "rev-TabsItem-panel--selected"}`}
-          >
-            <div className="rev-Row">
-              <div className="rev-Col">
-                <LiveEditor className="example-react-code" />
-              </div>
-            </div>
-          </div>
-          <div
-            className={`rev-TabsItem-panel ${active === 2 && "rev-TabsItem-panel--selected"}`}
-          >
-            <div className="rev-Row">
-              <div className="rev-Col">
-                <LiveHTML />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      </LiveProvider>
+          <Tabs.Stateful>
+            <Tabs.Item contentKey={1} title="React">
+              <Row>
+                <Col>
+                  <LiveEditor className="example-react-code" />
+                </Col>
+              </Row>
+            </Tabs.Item>
+            <Tabs.Item contentKey={2} title="HTML">
+              <Row>
+                <Col>
+                  <LiveHTML />
+                </Col>
+              </Row>
+            </Tabs.Item>
+          </Tabs.Stateful>
+        </LiveProvider>
       </div>
     )
   } else {
@@ -89,7 +74,6 @@ export default function ExampleSection(props) {
             examples={props.examples[examplesTitle]}
             scope={props.scope}
             key={examplesTitle}
-            disableHTMLPreview={props.disableHTMLPreview}
           />
         )
       }
