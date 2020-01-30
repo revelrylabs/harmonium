387bd5423a81e2f0ce7ae539d645ebc31cce55c2
Bryan Joseph <bryanjos@users.noreply.github.com>
2020-01-29 21:54:46 -0600

    chore: Put React and HTML output into tabs
    Replace component-playground with react-live. This is made by
    FormidableLabs as well and is more supported. And allows for
    more customization of layout.

--- docs-src/src/ExampleSection.js
+++ docs-src/src/ExampleSection.js
@@ -1,53 +1,83 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import {Row, Col} from 'harmonium/lib/grid'
-import Playground from 'component-playground'
-import Prism from 'prismjs'
 import prettier from 'prettier/standalone';
 import prettierHTML from 'prettier/parser-html'
+import {
+  LiveProvider,
+  LiveEditor,
+  LiveError,
+  LivePreview,
+  withLive
+} from 'react-live'
+import theme from "prism-react-renderer/themes/palenight";
+import ReactDOMServer from "react-dom/server";
+import Highlight, { defaultProps } from "prism-react-renderer";
 
-// Use requestAnimationFrame to add in HTML previews
-function renderHTMLExamples(previewAreas){
-  function worker(index){
-    if(index == previewAreas.length){
-      return;
-    }
+function HTML({ live }) {
+  const Result = live.element;
+  const html = ReactDOMServer.renderToStaticMarkup(<Result />);
+  const formattedHTML = prettier.format(html, {parser: 'html', plugins: [prettierHTML]})
 
-    const previewArea = previewAreas[index];
-    const parent = previewArea.closest('.rev-Col')
-    const htmlPreviewArea = parent.querySelector('.htmlPreviewArea')
-    if(htmlPreviewArea){
-      htmlPreviewArea.textContent = prettier.format(previewArea.innerHTML, {parser: 'html', plugins: [prettierHTML]})
-      Prism.highlightElement(htmlPreviewArea);
-    }
-
-    var nextFunction = worker.bind(this, index + 1);
-    window.requestAnimationFrame(nextFunction);
-  }
-
-  var nextFunction = worker.bind(this, 0);
-  window.requestAnimationFrame(nextFunction);
+  return (
+    <Highlight {...defaultProps} code={formattedHTML} language="markup" theme={theme}>
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+  );
 }
 
+const LiveHTML = withLive(HTML);
+
 export default function ExampleSection(props) {
-  useEffect(() => {
-    if(!props.disableHTMLPreview){
-      renderHTMLExamples(document.querySelectorAll('.previewArea'))
-    }
-  });
+  const [active, setActive] = useState(1);
 
   if (typeof props.examples === 'string') {
     return (
-      <React.Fragment>
-        <Playground theme="lucario" collapsableCode={true} codeText={props.examples} scope={props.scope} />
-        {
-          props.disableHTMLPreview ? null : (<section className="HTMLCodeSection">
-          <h3>HTML</h3>
-          <pre>
-            <code className="language-markup htmlPreviewArea"></code>
-          </pre>
-        </section>)
-        }
-      </React.Fragment>
+      <div className="example-section HTMLCodeSection">
+        <LiveProvider theme={theme} code={props.examples} scope={props.scope}>
+          <LiveError />
+          <LivePreview className="example-preview" />
+          <div className="rev-Tabs">
+        <ul className="rev-Tabs-titles">
+          <li className={`rev-TabsTitle ${active === 1 && "rev-TabsTitle--selected"}`}>
+            <a className="rev-TabsTitle-link" onClick={() => setActive(1)}>React</a>
+          </li>
+          <li className={`rev-TabsTitle ${active === 2 && "rev-TabsTitle--selected"}`}>
+            <a className="rev-TabsTitle-link" onClick={() => setActive(2)}>HTML</a>
+          </li>
+        </ul>
+        <div className="rev-Tabs-content">
+          <div
+            className={`rev-TabsItem-panel ${active === 1 && "rev-TabsItem-panel--selected"}`}
+          >
+            <div className="rev-Row">
+              <div className="rev-Col">
+                <LiveEditor className="example-react-code" />
+              </div>
+            </div>
+          </div>
+          <div
+            className={`rev-TabsItem-panel ${active === 2 && "rev-TabsItem-panel--selected"}`}
+          >
+            <div className="rev-Row">
+              <div className="rev-Col">
+                <LiveHTML />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </LiveProvider>
+      </div>
     )
   } else {
     const children = []
