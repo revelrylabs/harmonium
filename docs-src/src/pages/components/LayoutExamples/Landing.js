7ecbeb1c8948f6310b95d0bacab35f3b3fad7efb
Blaze Barsamian <blaze@revelry.co>
2020-03-18 14:28:01 -0500

    feature: cta example sections
    Connects to #516
    
    CTA section examples.
    - theres an issue with p text not being centered when `Text-center` is used (bc of paragraph max-width) but Laura has a fix for that in her branch.

--- docs-src/src/pages/components/LayoutExamples/Landing.js
+++ docs-src/src/pages/components/LayoutExamples/Landing.js
@@ -6,6 +6,9 @@ import Layout from '../../../layouts/index.js'
 
 const examples = {
   Basic: require('raw-loader!../../../examples/LayoutExamples/LandingHero.js.example'),
+  CtaDefault: require('raw-loader!../../../examples/LayoutExamples/CtaDefault.js.example'),
+  CtaSplit: require('raw-loader!../../../examples/LayoutExamples/CtaSplit.js.example'),
+  CtaPrimary: require('raw-loader!../../../examples/LayoutExamples/CtaPrimary.js.example'),
 }
 
 export default class LandingExamplePage extends Component {
