f028128ec3bd69d5e7f67760cd0146cf10cc71e8
Nick Schello <nick@revelry.co>
2022-06-05 12:41:18 -0400

    BREAKING: update to node 16

--- docs-src/gatsby-config.js
+++ docs-src/gatsby-config.js
@@ -48,9 +48,5 @@ module.exports = {
         },
       },
     },
-    //`gatsby-transformer-remark`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
