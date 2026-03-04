8e29fc98e8b58f2cf33c45377626cd73b945a1ff
gerardramos <gerard@revelry.co>
2026-03-04 09:40:15 -0600

    gh pages deployment and storybook config

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -2,7 +2,7 @@ import type {StorybookConfig} from '@storybook/react-vite'
 import {resolve} from 'path'
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.stories.@(ts|tsx)'],
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: {
     name: '@storybook/react-vite',
